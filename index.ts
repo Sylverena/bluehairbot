cab442b2f43262c6456804a7b5f9af981efe95ca
Serena <serenazsyl@gmail.com>
2024-06-25 12:11:42 -0500

    Completed bot login and amended gitignore

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-require('dotenv').config();
-console.log(process.env.TOKEN);
+require('dotenv').config(); // Require dotenv for token storage
 
+// Pull token from env and necessary classes from discord.js
+const token = process.env.TOKEN;
+import {Client, Events, GatewayIntentBits} from 'discord.js';
+
+// Create client instance
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+// When the client is ready, run this code (only once).
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token).then();
